3da3550f35fa12bbe162cbc24280b61257639e7d
NadaSamy <nadaasamy369@gmail.com>
2022-05-16 02:29:13 +0200

    Register validation and adding new users to DB completed

--- careerFinder/app/Http/Controllers/UserController.php
+++ careerFinder/app/Http/Controllers/UserController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Users;
+use Illuminate\Support\Facades\Hash;
+
 class UserController extends Controller
 {
     public function home()
@@ -19,7 +22,26 @@ class UserController extends Controller
         return view('login');
     }
     public function register_action(Request $request){
-
+        // validate requests
+        $request->validate([
+            'name' => 'required||max:30',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:5|max:12',
+            'confirmpassword' => 'required|same:password',
+        ]);
+        // insert data into database
+        $user = new Users([
+            'email' => $request->email,
+            'name' => $request->name,
+            'password' => Hash::make($request->password),
+            'role' => 3,
+        ]);
+        $save = $user->save();
+        if($save){
+            return redirect()->route('login')->with('success', 'Registration success. Please login!');
+        }else{
+            return back()->with('fail', 'Something went wrong try again later!');
+        }
     }
     public function login_action(Request $request){
 
--- careerFinder/resources/views/register.blade.php
+++ careerFinder/resources/views/register.blade.php
@@ -16,6 +16,14 @@
     <form action="{{ route('register.action') }}" method="POST">
         @csrf
         <h1 class="heading">Register</h1>
+        @if(Session::get('fail'))
+        <div class="danger">{{ Session::get('fail') }}</div>
+        @endif
+        @if($errors->any())
+            @foreach($errors->all() as $err)
+            <div class="danger">{{ $err }}</div>
+            @endforeach
+        @endif
         <div class="inputBox">
             <input type="text" name="name">
             <label for="name">name<span class="requiredF">*</span></label>
@@ -33,7 +41,7 @@
             <label for="confirmpassword">confirm password<span class="requiredF">*</span></label>
         </div>
         <p>Have an account? <a href="{{ route('login') }}" class="alignA">LogIn</a>.</p>
-        <input type="submit" class="btn" value="sign up">
+        <input type="submit" class="btn" value="Register">
     </form>
 </section>
 @endsection
