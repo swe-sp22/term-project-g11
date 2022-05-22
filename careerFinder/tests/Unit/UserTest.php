49ce2437d51e47dbb63cad761b1fb32f085632ad
Ahmed Elselmy <ahmedelselmy@Ahmeds-MacBook-Air.local>
2022-05-22 20:53:50 +0200

    html editing

--- careerFinder/tests/Unit/UserTest.php
+++ careerFinder/tests/Unit/UserTest.php
@@ -16,24 +16,7 @@ class UserTest extends TestCase
      * @return void
      */
 
-    //Checking validation
     
-    public function test_database()
-    {
-        $this->assertDatabaseHas('users',[
-            'name' =>'ahmed elselmy'
-        ]);    
-    }
-
-    // check wether password and confirm password are equal
-    public function test_user_password()
-    {
-        $confirmPass = 'John Doe';
-        $user1 = Users::make([
-            'password' => 'John Doe',
-        ]);
-        $this->assertTrue($user1->password == $confirmPass);
-    }
     // check if email is not taken
     public function test_database_email()
     {
@@ -42,31 +25,37 @@ class UserTest extends TestCase
             'email' => $userEmail
         ]);    
     }
-    // check the validation of the email format
-    public function test_email_validation()
-    {
-        $request = 'aa@hotmail.com';
-        $this->assertTrue(!preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $request) == FALSE);
-    }
-    public function test_register(){
+
+    public function test_register_name(){
         $response = [
-            'name' => 'merna',
+            'name' => '',
             'email' => 'selmyy@gmail.com',
             'password' => 'merna10iii',
             'confirmpassword' => 'merna10iii',
         ];
         //$this->post(route('register.action'),$response)->assertSessionHasErrors(['email']);
+        $this->post(route('register.action'),$response)->assertSessionDoesntHaveErrors(['name']);
+    }
+    public function test_register_pass(){
+        $response = [
+            'name' => 'merna',
+            'email' => 'selmyy@gmail.com',
+            'password' => '10iii',
+            'confirmpassword' => 'mna10iii',
+        ];
+        //$this->post(route('register.action'),$response)->assertSessionHasErrors(['email']);
         $this->post(route('register.action'),$response)->assertSessionDoesntHaveErrors();
     }
-    // public function test_register(){
-    //     $response = [
-    //         'name' => 'merna',
-    //         'email' => 'selmmy@gmail.com',
-    //         'password' => 'merna10iii',
-    //         'confirmpassword' => 'merna10iii',
-    //     ];
-    //     //$this->post(route('register.action'),$response)->assertSessionHasErrors(['email']);
-    //     $this->post(route('register.action'),$response)->assertSessionDoesntHaveErrors(['email']);
-    // }
+    public function test_register_email(){
+        $response = [
+            'name' => 'merna',
+            'email' => 'selmil.com',
+            'password' => 'mna10iii',
+            'confirmpassword' => 'mna10iii',
+        ];
+        //$this->post(route('register.action'),$response)->assertSessionHasErrors(['email']);
+        $this->post(route('register.action'),$response)->assertSessionDoesntHaveErrors(['email']);
+    }
+    
 
 }
